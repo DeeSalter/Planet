9e636e58d1868a63201f62e9d273ccf1aeed2092
Diane Salter <dianesalternew@gmail.com>
2014-06-14 17:00:58 -0700

    initial commit

---
+++ OWOuterSpaceTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  OWOuterSpaceTableViewController.h
+//  Out of This World
+//
+//  Created by Diane Salter on 6/14/14.
+//  Copyright (c) 2014 Diane Salter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWOuterSpaceTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *planets;
+
+@end
